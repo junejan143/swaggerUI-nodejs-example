900b8f52a83d620fd419d089a23823537c77be6f
章源辰 <zhangyuanchen143@163.com>
2016-11-07 23:02:24 +0800

    swaggerUI使用nodejs提供服务

---
+++ server.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var multer  = require('multer');
+var fs = require("fs");
+
+var app = express();
+var admin = express();
+
+app.use('/static', express.static('public'));
+app.use('/admin',admin);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(multer({ dest: '/tmp/'}).array('file'));
+
+admin.get('/login.htm', function (req, res) {
+   res.sendFile( __dirname + "/" + "login.htm" );
+})
+
+app.post('/file_upload', function (req, res) {
+   console.log(req.files[0]);  // 上传的文件信息
+   var des_file = __dirname + "/public/" + req.files[0].originalname;
+   console.log(des_file);
+   fs.readFile( req.files[0].path, function (err, data) {
+        fs.writeFile(des_file, data, function (err) {
+         if( err ){
+              console.log( err );
+         }else{
+               response = {
+                   message:'File uploaded successfully',
+                   filename:req.files[0].originalname
+              };
+          }
+          console.log( response );
+          res.end( JSON.stringify( response ) );
+       });
+   });
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("应用实例，访问地址为 http://%s:%s", host, port)
+  console.log('Example app listening on port 3000!');
+});
